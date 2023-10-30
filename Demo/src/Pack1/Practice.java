54c3db3d2b186724c083755590b2c0780977561d
N.Sharvani <nsharvani2021@gmail.com>
2023-10-30 11:58:00 +0530

    Second commit of pack1

--- Demo/src/Pack1/Practice.java
+++ Demo/src/Pack1/Practice.java
@@ -5,6 +5,7 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Hello Welcome to G.P.R.E.C");
+       System.out.println("My self Sai Sharvani");
 	}
 
 }
