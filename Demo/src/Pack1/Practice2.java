a503a01711d40b300d8656711b549cd28af284a2
N.Sharvani <nsharvani2021@gmail.com>
2023-10-30 11:50:14 +0530

    second commit

---
+++ Demo/src/Pack1/Practice2.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int n1=3;
+         int n2=4;
+         System.out.println(n1+n2);
+	}
+
+}
