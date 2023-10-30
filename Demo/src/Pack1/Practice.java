fc5703b80f1b96b0de3b7a27b66fa051651c3562
N.Sharvani <nsharvani2021@gmail.com>
2023-10-30 11:21:59 +0530

    first commit

---
+++ Demo/src/Pack1/Practice.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello Welcome to G.P.R.E.C");
+	}
+
+}
